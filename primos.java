124225a5fdf1a29180399a4c779ef66f0790784f
yaitox <47457721+yaitox@users.noreply.github.com>
2019-03-29 13:57:00 -0500

    parte 3 tercero

--- primos.java
+++ primos.java
@@ -5,17 +5,30 @@ public class is_prime {
 		// TODO Auto-generated method stub
 Scanner scanner = new Scanner(System.in);
 int a = scanner.nextInt();
-		if(a<=0)
-			break;
 primo(a);
 	}
 	public static void primo(int x) {
+		
 		try {
-		int contCero=0;
+		int contPrimos=0;
+			int contCero=0;
 		for(int i=1;i<=x;i++) {
+			
 			if(x%i==0)
 				contCero++;
+			if(contCero==2)
+				contPrimos++;
+			
 		}
+System.out.println("Se calcularon "+contPrimos+" primos");
+int contPrimoz=0;
+for(int i=1;i<=contPrimos;i++) {
+	if(contPrimos%i==0)
+		contPrimoz++;
+		
+}
+if(contPrimoz==0)
+	System.out.println("es primo: "+contPrimos);
 		if(contCero==2)
 			System.out.println("-1");
 		else
@@ -24,6 +37,6 @@ primo(a);
 			System.out.println("No se puede determinar");
 			
 		}
+		
 		}
 
-}
